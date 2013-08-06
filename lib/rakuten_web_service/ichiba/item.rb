aef955e43e0a971764a195b308ba3bee11a99862
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2013-08-06 14:35:30 +0900

    RWS::Ichiba::Item.search send a request to search items

--- lib/rakuten_web_service/ichiba/item.rb
+++ lib/rakuten_web_service/ichiba/item.rb
@@ -1,8 +1,43 @@
+require 'faraday'
+
 module RakutenWebService
   module Ichiba
+    class Client
+      attr_reader :url, :path
+
+      def initialize(endpoint)
+        url = URI.parse(endpoint)
+        @url = "#{url.scheme}://#{url.host}"
+        @path = url.path
+      end
+
+      def get(query)
+        connection.get(path, query)
+      end
+
+      private
+      def connection
+        return @connection if @connection
+        @connection = Faraday.new(:url => url) do |conn|
+          conn.request :url_encoded
+          conn.adapter Faraday.default_adapter
+        end
+      end
+    end
+
     class Item
       class << self
         def search(options)
+          client = Client.new(endpoint)
+          client.get(options)
+        end
+
+        private
+        def endpoint
+          'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20120805'
+        end
+
+        def connection
         end
       end
     end
