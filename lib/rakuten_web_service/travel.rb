e57684ec0e717b09f5533109cfa43a16cebde7f3
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2014-02-20 17:16:34 +0900

    Add RakutenWebService::Travel::Hotel to provide SimpleHotelSearch API

--- lib/rakuten_web_service/travel.rb
+++ lib/rakuten_web_service/travel.rb
@@ -1,1 +1,2 @@
 require 'rakuten_web_service/travel/area_class'
+require 'rakuten_web_service/travel/hotel'
---
+++ lib/rakuten_web_service/travel/hotel.rb
@@ -0,0 +1,23 @@
+require 'rakuten_web_service/resource'
+
+module RakutenWebService
+  module Travel
+    class Hotel < RakutenWebService::Resource
+      endpoint 'https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20131024'
+
+      set_parser do |response|
+        response['hotels'].map do |hotel_info|
+          Hotel.new(hotel_info)
+        end
+      end
+
+      def initialize(params)
+        @params = {}
+        @params['hotelBasicInfo'] = params['hotel'][0]['hotelBasicInfo']
+        @params['hotelRatingInfo'] = params['hotel'][1]['hotelRatingInfo']
+      end
+
+      attribute :hotelBasicInfo, :hotelRatingInfo
+    end
+  end
+end
