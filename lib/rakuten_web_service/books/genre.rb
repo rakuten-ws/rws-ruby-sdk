0da95143cd0703234ba6d90a43e27726ae9227d4
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2014-09-28 01:31:41 +0900

    Change the superclass of Books::Genre to RWS::BaseGenre

--- lib/rakuten_web_service/books/genre.rb
+++ lib/rakuten_web_service/books/genre.rb
@@ -1,26 +1,11 @@
-require 'rakuten_web_service/resource'
+require 'rakuten_web_service/genre'
 
 module RakutenWebService
   module Books
-    class Genre < Resource
+    class Genre < RakutenWebService::BaseGenre
       set_resource_name 'books_genre'
 
       endpoint 'https://app.rakuten.co.jp/services/api/BooksGenre/Search/20121128'
-
-      set_parser do |response|
-        current = response['current']
-        if children = response['children']
-          children = children.map { |child| Books::Genre.new(child['child']) }
-          current.merge!('children' => children)
-        end
-        if parents = response['parents']
-          parents = parents.map { |parent| Books::Genre.new(parent['parent']) }
-          current.merge!('parents' => parents)
-        end
-
-        genre = Books::Genre.new(current)
-        [genre]
-      end
 
       attribute :booksGenreId, :booksGenreName, :genreLevel
 
@@ -31,20 +16,12 @@ module RakutenWebService
       def self.new(params)
         case params
         when String
-          Genre[params] ||= self.search(:booksGenreId => params).first
+          self[params] ||= self.search(:booksGenreId => params).first
         when Hash
           super
         else
           raise ArgumentError, 'Invalid parameter for initializing Books::Genre'
         end
-      end
-
-      def self.[](id)
-        repository[id]
-      end
-
-      def self.[]=(id, genre)
-        repository[id] = genre
       end
 
       def children
