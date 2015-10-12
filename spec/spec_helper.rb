84544bbcab53fe5fa33911e0055ab96d4543c673
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2015-10-12 18:00:08 +0900

    Fix env var name for CodeClimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'webmock/rspec'
 require 'tapp'
 
-if ENV['CODE_CLIMATE_TOKEN']
+if ENV['CODECLIMATE_REPO_TOKEN']
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
 end
