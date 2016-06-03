0df469c341af91f2a3cd14e4bed302fa6716ccd2
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-06-03 17:33:37 +0900

    Add an integration test case to confirm AllProxy works well

--- spec/integration/rakuten_web_service_ichiba_spec.rb
+++ spec/integration/rakuten_web_service_ichiba_spec.rb
@@ -5,8 +5,18 @@ describe RakutenWebService::Ichiba, integration: true do
     let(:params) { { keyword: 'Rakuten' } }
     let(:items) { RakutenWebService::Ichiba::Item.search(params) }
 
-    specify 'should search without error' do
-      expect { items.first }.to_not raise_error
+    describe '#first' do
+      specify 'should search without error' do
+        expect { items.first }.to_not raise_error
+      end
+    end
+    describe '#all' do
+      specify 'should respond all resources without error' do
+        expect { items.all }.to_not raise_error
+      end
+      specify 'each resource should respond attributes' do
+        expect(items.all).to be_all(&:name)
+      end
     end
   end
 end
