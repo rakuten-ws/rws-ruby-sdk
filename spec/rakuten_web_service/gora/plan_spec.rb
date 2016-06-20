644ea355047a1ddb6a8e4abf56e23e7187ecc0d1
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-06-20 17:18:16 +0900

    Replace the deprecated method RWS.configuration

--- spec/rakuten_web_service/gora/plan_spec.rb
+++ spec/rakuten_web_service/gora/plan_spec.rb
@@ -16,7 +16,7 @@ describe RakutenWebService::Gora::Plan do
   end
 
   before do
-    RakutenWebService.configuration do |c|
+    RakutenWebService.configure do |c|
       c.affiliate_id = affiliate_id
       c.application_id = application_id
     end
