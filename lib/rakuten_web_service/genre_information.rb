73a38551ab83a3cde09ec8ac63ff7ef414ecd5ba
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-09-27 13:44:01 +0900

    GenreInformation#children's each element has Genre attribute

--- lib/rakuten_web_service/genre_information.rb
+++ lib/rakuten_web_service/genre_information.rb
@@ -1,6 +1,11 @@
 module RakutenWebService
   class GenreInformation
+    attr_reader :parent, :current, :children
+
     def initialize(params)
+      @parent = Array(params['parent']).first
+      @current = Array(params['current']).first
+      @children = params['children'].map { |child| RWS::Ichiba::Genre.new(child['child']) }
     end
   end
 end
--- spec/rakuten_web_service/genre_information_spec.rb
+++ spec/rakuten_web_service/genre_information_spec.rb
@@ -10,7 +10,7 @@ describe RakutenWebService::GenreInformation do
           {
             "child" => {
               "genreId" => "564500",
-              "genereName" => "光回線・モバイル通信",
+              "genreName" => "光回線・モバイル通信",
               "itemCount" => "5",
               "genreLevel" => "1"
             }
@@ -22,5 +22,28 @@ describe RakutenWebService::GenreInformation do
     subject { RakutenWebService::GenreInformation.new(expected_params) }
 
     it { is_expected.to be_a(RakutenWebService::GenreInformation) }
+    specify "its parent should be nil" do
+      expect(subject.parent).to be_nil
+    end
+    specify "its current should be nil" do
+      expect(subject.current).to be_nil
+    end
+    specify "its children should returns a array of Genre" do
+      expect(subject.children).to_not be_empty
+    end
+
+    describe "each child" do
+      subject { super().children.first }
+
+      it "has genre id" do
+        expect(subject.id).to eq('564500')
+      end
+      it "has genre name" do
+        expect(subject.name).to eq('光回線・モバイル通信')
+      end
+      it "has its genre level" do
+        expect(subject.level).to eq('1')
+      end
+    end
   end
 end
