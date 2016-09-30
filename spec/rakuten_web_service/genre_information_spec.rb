cd04102c6f6e62b27877d8289a925c1c94e3b089
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-09-30 19:00:51 +0900

    Add an example of GenreInformation to check its `current`

--- spec/rakuten_web_service/genre_information_spec.rb
+++ spec/rakuten_web_service/genre_information_spec.rb
@@ -2,50 +2,79 @@ require 'spec_helper'
 
 describe RakutenWebService::GenreInformation do
   describe '#initialize' do
-    let(:expected_params) do
-      {
-        "parent" => [],
-        "current" => [],
-        "children" => [
-          {
-            "child" => {
-              "genreId" => "564500",
-              "genreName" => "光回線・モバイル通信",
-              "itemCount" => "5",
-              "genreLevel" => "1"
-            }
-          }
-        ]
-      }
-    end
-
     subject { RakutenWebService::GenreInformation.new(expected_params) }
 
-    it { is_expected.to be_a(RakutenWebService::GenreInformation) }
-    specify "its parent should be nil" do
-      expect(subject.parent).to be_nil
-    end
-    specify "its current should be nil" do
-      expect(subject.current).to be_nil
-    end
-    specify "its children should returns a array of Genre" do
-      expect(subject.children).to_not be_empty
-    end
+    context "When given params only has children genres" do
+      let(:expected_params) do
+        {
+          "parent" => [],
+          "current" => [],
+          "children" => [
+            {
+              "child" => {
+                "genreId" => "564500",
+                "genreName" => "光回線・モバイル通信",
+                "itemCount" => "5",
+                "genreLevel" => "1"
+              }
+            }
+          ]
+        }
+      end
 
-    describe "each child" do
-      subject { super().children.first }
+      it { is_expected.to be_a(RakutenWebService::GenreInformation) }
+      specify "its parent should be nil" do
+        expect(subject.parent).to be_nil
+      end
+      specify "its current should be nil" do
+        expect(subject.current).to be_nil
+      end
+      specify "its children should returns a array of Genre" do
+        expect(subject.children).to_not be_empty
+      end
 
-      it "has genre id" do
-        expect(subject.id).to eq('564500')
+      describe "each child" do
+        subject { super().children.first }
+
+        it "has genre id" do
+          expect(subject.id).to eq('564500')
+        end
+        it "has genre name" do
+          expect(subject.name).to eq('光回線・モバイル通信')
+        end
+        it "has its genre level" do
+          expect(subject.level).to eq('1')
+        end
+        it "has its item count" do
+          expect(subject.item_count).to eq('5')
+        end
       end
-      it "has genre name" do
-        expect(subject.name).to eq('光回線・モバイル通信')
+    end
+    context "When given params has current genre" do
+      let(:expected_params) do
+        {
+          "parent" => [],
+          "current" => [
+            {
+              "genreId" => "564500",
+              "genreName" => "スマートフォン・タブレット",
+              "itemCount" => "313045",
+              "genreLevel" => "1"
+            }
+          ],
+          "children" => [
+          ]
+        }
       end
-      it "has its genre level" do
-        expect(subject.level).to eq('1')
+
+      specify "its parent should be nil" do
+        expect(subject.parent).to be_nil
       end
-      it "has its item count" do
-        expect(subject.item_count).to eq('5')
+      specify "its current should be a Genre object" do
+        expect(subject.current).to be_a(RWS::Ichiba::Genre)
+      end
+      specify "its children should be empty" do
+        expect(subject.children).to be_empty
       end
     end
   end
