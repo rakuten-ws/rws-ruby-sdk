7c12e6875067c0097786236629cdcbdbcfcf10b2
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-03-04 10:31:21 +0900

    Remove duplicated definition of has_next_page?

--- lib/rakuten_web_service/travel/search_result.rb
+++ lib/rakuten_web_service/travel/search_result.rb
@@ -3,11 +3,6 @@ require 'rakuten_web_service/search_result'
 module RakutenWebService
   module Travel
     class SearchResult < RakutenWebService::SearchResult
-      def has_next_page?
-        false unless paging_info
-        paging_info['page'] && paging_info['page'] < paging_info['pageCount']
-      end
-
       def params_to_get_next_page
         @params.merge('page' => (paging_info['page'] + 1))
       end
